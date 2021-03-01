2c7ad30f0fe636e32d682f7930910062429e22c6
carriebarrett <67287344+carriebarrett@users.noreply.github.com>
2021-02-28 17:40:55 -0800

    Add files via upload

---
+++ toDoList.js
@@ -0,0 +1,85 @@
+// Carrie Barrett
+// CS 290
+
+// start with setup code from lecture
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5357);
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var session = require('express-session');
+app.use(session({secret:'DoNotTell'}));
+
+// basic to do list code from lecture
+app.get('/',function(req,res,next){
+  var context = {};
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newSession', context);
+    return;
+  }
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length || 0;
+  context.toDo = req.session.toDo || [];
+  console.log(context.toDo);
+  res.render('toDo',context);
+});
+
+app.post('/',function(req,res){
+  var context = {};
+
+  if(req.body['New List']){
+    req.session.name = req.body.name;
+    req.session.toDo = [];
+    req.session.curId = 0;
+  }
+
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newSession', context);
+    return;
+  }
+
+  if(req.body['Add Item']){
+    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
+    req.session.curId++;
+  }
+
+  if(req.body['Done']){
+    req.session.toDo = req.session.toDo.filter(function(e){
+      return e.id != req.body.id;
+    })
+  }
+
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length;
+  context.toDo = req.session.toDo;
+  console.log(context.toDo);
+  res.render('toDo',context);
+});
+
+// setup code from lecture continues
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
