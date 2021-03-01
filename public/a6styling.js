08bf52e303c2896ae247bf095e5a6f3287c0dc2a
Carrie Barrett <67287344+carriebarrett@users.noreply.github.com>
2021-03-01 13:00:05 -0800

    Update a6styling.js

--- public/a6styling.js
+++ public/a6styling.js
@@ -1,6 +1,6 @@
 // front end of A6
 document.getElementById("addColor").addEventListener("click", colorHeadings);
-function colorHeadings {
+function colorHeadings() {
 	var itemList = document.getElementsByTagName("h3");
 	for (var i=0, i<itemList.length, i++){
 		itemList[i].style.backgroundColor = "yellow";
